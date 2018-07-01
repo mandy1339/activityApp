b3e39da96f2e7c3f78876122a1d97b9389ca8390
EBRIDGE\atoledo <armando.toledo@enterbridge.com>
2018-07-01 14:44:06 -0400

    dump file for activity table

---
+++ db/dumps/07_01_18/activityappdb_activity.sql
@@ -0,0 +1,37 @@
+-- MySQL dump 10.13  Distrib 8.0.11, for Win64 (x86_64)
+--
+-- Host: 127.0.0.1    Database: activityappdb
+-- ------------------------------------------------------
+-- Server version	8.0.11
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+ SET NAMES utf8 ;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Dumping data for table `activity`
+--
+
+LOCK TABLES `activity` WRITE;
+/*!40000 ALTER TABLE `activity` DISABLE KEYS */;
+INSERT INTO `activity` VALUES (1,'Father\'s Day Cville restaurant','2018-06-16','Went to Buffalo Wild Wings with Rudy for father\'s day. Andrea, Nathan and Sonia were there.',1,1,0,1),(2,'Movies night','2018-06-15','Went to the movies 30 mins away to watch Oceani\'s 8.',0,1,0,0),(3,'qqwer','2018-05-05','213sdaf sadfsjaldfk ',1,1,1,1),(4,'213','2018-06-29','2132143',0,0,0,0),(5,'213','2018-06-29','2132143',0,0,0,0),(6,'213','2018-06-29','2132143',0,0,0,0),(7,'213','2018-06-29','2132143',0,0,0,0),(8,'213','2018-06-29','2132143',0,0,0,0),(9,'213','2018-06-29','2132143',0,0,0,0),(10,'xzcv','2018-06-23','xzcvxzcvxvxcvxzcv',0,0,0,0),(11,'activity 323','2018-06-27','this is activity 323 long description',0,0,0,0),(12,'1234','2018-06-15','21342134',0,0,0,0),(13,'fsd gsadf ','2018-06-20','sadf sadf asd fsad f',0,0,0,0),(14,'sadf ','2018-06-01','sad fsad f',0,0,0,0),(15,'235','2018-06-15','25235235',0,0,0,0);
+/*!40000 ALTER TABLE `activity` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2018-07-01 14:40:28
